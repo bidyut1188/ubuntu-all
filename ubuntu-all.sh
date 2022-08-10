dd90ed14e681424fc95f3f0b906fcda6167560cb
bidyut1188 <bsengupta1188@gmail.com>
2022-08-10 11:04:43 +0530

    First Commit

---
+++ ubuntu-all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# Check sudo priv.
+# Do not require sudo with android
+if ! [ "$(uname -o)" = Android ]; then
+	SUDO=
+else
+	SUDO=sudo
+fi
+
+# Do not require sudo when already root
+if [ "$(id -u)" = 0 ]; then
+	SUDO=
+else
+	SUDO=sudo
+fi
+
+echo "UBUNTU-ALL made by proot"
+sleep 3
+echo "Trying update..."
+$SUDO apt update
+echo "Trying install proot..."
+$SUDO apt install proot -y
+
+for ver in '0.1 0.3 0.7 1.1 1.3 1.4 1.5'; do
+	cd ubuntu-in-termux-$ver
+	bash ubuntu.sh
+	cd ..
+done
+
