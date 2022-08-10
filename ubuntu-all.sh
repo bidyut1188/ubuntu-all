8026fda82fb8fdbdcc45796e423564bf2df81596
bidyut1188 <bsengupta1188@gmail.com>
2022-08-10 11:12:31 +0530

    Fix ubuntu-all

--- ubuntu-all.sh
+++ ubuntu-all.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x 
 set -e
 
 # Check sudo priv.
-# Do not require sudo with android
-if ! [ "$(uname -o)" = Android ]; then
+# Do not require sudo when already root
+if [ "$(id -u)" = 0 ]; then
 	SUDO=
 else
 	SUDO=sudo
 fi
 
-# Do not require sudo when already root
-if [ "$(id -u)" = 0 ]; then
+# Do not require sudo with android
+if [ "$(uname -o)" = Android ]; then
 	SUDO=
 else
 	SUDO=sudo
@@ -23,7 +23,7 @@ $SUDO apt update
 echo "Trying install proot..."
 $SUDO apt install proot -y
 
-for ver in '0.1 0.3 0.7 1.1 1.3 1.4 1.5'; do
+for ver in 0.1 0.3 0.7 1.1 1.3 1.4 1.5; do
 	cd ubuntu-in-termux-$ver
 	bash ubuntu.sh
 	cd ..
